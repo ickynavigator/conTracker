c54ebe91e086f3586e8c0cc1541956282ddc230c
Obi Fortune <gabrielobi.of@gmail.com>
2021-11-10 15:47:41 +0200

    basic structure for the backend folder - error middleware

---
+++ packages/con-tracker-api/routes/index.js
@@ -0,0 +1,2 @@
+const allRoutes = {}
+export default allRoutes
---
+++ packages/con-tracker-api/server.js
@@ -0,0 +1,37 @@
+// import path from "path"
+import express from "express";
+import dotenv from "dotenv";
+import "colors";
+import morgan from "morgan";
+
+import { notFound, errorHandler } from "./middleware/errorMiddleware";
+import { connectMONGO } from "./db/conn";
+import allRoutes from "./routes/index";
+
+dotenv.config();
+
+const PORT = process.env.API_PORT || 5000;
+connectMONGO();
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use("/api", allRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(
+  PORT,
+  console.log(
+    `\nServer running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold,
+  ),
+);
